413265ded0923f1965a07e80e151e00f8267ed98
saba_zedginidze <saba_zedginidze@epam.com>
2025-04-23 22:01:38 +0400

    Add html pages

--- src/main/java/edu/kiu/midterm/MainController.java
+++ src/main/java/edu/kiu/midterm/MainController.java
@@ -1,27 +1,46 @@
 package edu.kiu.midterm;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class MainController {
 
 	@GetMapping("/health")
+	@ResponseBody
 	public void health() {
 		System.out.println("Health check passed");
 	}
 
 	@GetMapping("/hello/{name}")
+	@ResponseBody
 	public String hello(@PathVariable("name") String name) {
 		return "Hello, " + name + "!";
 	}
 
 	@PostMapping("/form")
+	@ResponseBody
 	public String submit(@RequestParam("name") String name) {
 		return "Form submitted with name: " + name;
+	}
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/form")
+	public String form() {
+		return "form";
+	}
+
+	@GetMapping("/status")
+	public String status() {
+		return "status";
 	}
 
 }
